a2bf3c5161131e753e60bca4f5d67a7cefec5f2b
Yumna Akhtar <yumnaakhtar@hotmail.com>
2018-05-28 13:49:40 -0700

    complete add to inventory function

--- bamazonManager.js
+++ bamazonManager.js
@@ -16,13 +16,13 @@ connection.connect(function (err) {
     start();
 });
 
-function start() {
+function start(result) {
     inquirer
         .prompt({
             name: "action",
             type: "list",
             message: "What would you like to do?",
-            choices: ["View Products for Sale", "View Low Inventory", "Add to Inventory", "Add New Product"]
+            choices: ["View Products for Sale", "View Low Inventory", "Add to Inventory", "Add New Product", "Exit"]
         })
         .then(function (answer) {
             switch (answer.action) {
@@ -33,9 +33,7 @@ function start() {
                     printLow();
                     break;
                 case "Add to Inventory":
-                    // If a manager selects Add to Inventory, your app should display a prompt that will let the manager "add more" of any item currently in the store.
-                    console.log("you chose option 3");
-                    addInventory();
+                    addInventory(result);
                     break;
                 case "Add New Product":
                     // If a manager selects Add New Product, it should allow the manager to add a completely new product to the store.
@@ -43,6 +41,8 @@ function start() {
                     addProduct();
                     start();
                     break;
+                case "Exit":
+                    connection.end();
             }
 
         });
@@ -55,7 +55,7 @@ function printAvailable() {
                 console.log("id: " + res[i].id + " item: " + res[i].item + " department: " + res[i].department + " price: " + res[i].price + " stock: " + res[i].stock);
             }
         }
-        start();
+        start(res);
     });
 }
 
@@ -71,9 +71,55 @@ function printLow() {
 }
 
 function addInventory(res) {
-    
+    inquirer
+        .prompt([
+            {
+                name: "item_ID",
+                type: "input",
+                message: "What is the item number of the product you'd like to add? ",
+                validate: function (value) {
+                    if (isNaN(value) === false) {
+                        return true;
+                    }
+                    console.log("\n Invalid input, please try again");
+                    return false;
+                }
+            },
+            {
+                name: "amount",
+                type: "input",
+                message: "How many would you like to add to inventory? ",
+                validate: function (value) {
+                    if (isNaN(value) === false) {
+                        return true;
+                    }
+                    console.log("\n Invalid input, please try again");
+                    return false;
+                }
+            }
+        ]).then(function (answer) {
+                connection.query(
+                    "UPDATE products SET ? WHERE ?",
+                    [
+                        {
+                            stock:
+                                (res[answer.item_ID - 1].stock + parseInt(answer.amount))
+                        },
+                        {
+                            id: answer.item_ID
+                        }
+                    ],
+                    function (err) {
+                        if (err) throw err;
+                        else {
+                            console.log("Inventory has been adjusted");
+                            start();
+                        }
+                    }
+                )
+        })
 }
 
 function addProduct() {
-    
+
 }
